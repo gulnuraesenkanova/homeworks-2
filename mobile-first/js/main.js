631ac172ad556a89538175693572a8763d6f3288
gulnura.esenkanova.pls@gmail.com <gulnur.esenkanova.pls@gmail.com>
2021-06-13 01:07:11 +0600

    1

--- mobile-first/js/main.js
+++
@@ -1,8 +0,0 @@
-
-const menuToggler = document.querySelector('.menu-toggler');
-
-const menu = document.querySelector('.menu');
-
-menuToggler.addEventListener('click', function () {
-  menu.classList.toggle('js-menu-show');
-})
